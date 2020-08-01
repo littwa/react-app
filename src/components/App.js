8088aa16a9f3a54adb0a0d713d7081ba7850e1d0
littwa <littwa777@gmail.com>
2020-08-01 22:50:46 +0300

    todoStart

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,31 @@
 import React from "react";
-import Product from "./Product";
-import Section from "./Section";
-import BookList from "./BookList";
-import favouriteBooks from "../books.json";
-// console.log(Product);
+import { uuid } from "uuidv4";
 
-export default function App() {
- return (
-  <>
-   <BookList books={favouriteBooks} />
-   <h1>Welcome!</h1>
-   <Section>
-    <Product
-     // imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-     name="Tacos With Lime"
-     price={10.99}
-     quantity={30}
-    />
-   </Section>
+class App extends React.Component {
+  state = {
+    inputValue: "",
+    toDoList: [],
+  };
 
-   <Section title="Recommended">
-    <Product
-     imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-     name="Fries and Burger"
-     price={30.49}
-     quantity={100}
-    />
-   </Section>
-  </>
- );
+  addTask = (name) => {
+    const taska = {
+      name,
+      id: uuid(),
+      completed: false,
+    };
+
+    this.setState((prevState) => {
+      return { toDoList: [...prevState.toDoList, taska] };
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div>2323</div>
+      </>
+    );
+  }
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+//===================================================================
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
+// console.log(React.Children);
+
+//====================Promise=TEST УДАЛИТЬ==========================================
+// let fff = async () => await (5 + 7 + 7);
+// fff().then((f) => console.log(f));
+
+// let promise = new Promise((resolve, reject) => {
+//   let d = 1;
+
+//   if (d > 0) {
+//     resolve("resolve!!!");
+//   } else {
+//     reject("reject!!!");
+//   }
+// });
+// promise.then((q) => console.log(q)).catch((q) => console.log(q));
+
+// //=====================Class=TEST УДАЛИТЬ===================================
+
+class Tehnica {
+  static speed = 999;
+  #priv = 100;
+  name = "Auto";
+  engine = "V8TDi";
+  p = this.#priv;
+
+  constructor(q = "q", e = 3, z = 1) {
+    if (this.#priv < Tehnica.speed) {
+      this.namett = 333 + z;
+    }
+    this.name = q;
+    this.engine = e;
+  }
+  foouu() {
+    console.log("Yhoo!");
+  }
+}
+
+class SuperTehnica extends Tehnica {
+  namerr = "AutoPlus";
+  constructor(v, y, u, o) {
+    super(v, o, u);
+    this.namexxx = y;
+  }
+}
+let g = new SuperTehnica();
+g.aa = 1;
+
+//===================================================================
