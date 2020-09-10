0668692fbb33586ef0024de538d2cabce8f09b8c
littwa <littwa777@gmail.com>
2020-09-10 14:44:06 +0300

    any

--- src/redux/ph/phReducer.js
+++ src/redux/ph/phReducer.js
@@ -2,7 +2,7 @@ import { createReducer } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import phAction from "./phAction";
 
-const red1 = createReducer(0, { sd: (s, a) => s + a });
+const redTest = createReducer(0, {});
 const contacts = createReducer([], { [phAction.act]: (s, a) => [...s, a.payload.contact] });
 
-export default combineReducers({ red1, contacts });
+export default combineReducers({ redTest, contacts });
