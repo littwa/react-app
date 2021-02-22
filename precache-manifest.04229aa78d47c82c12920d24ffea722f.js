86ba577d86deed275f112e24231fbbc7b2095786
littwa <littwa777@gmail.com>
2021-02-22 22:25:13 +0200

    Updates

---
+++ precache-manifest.04229aa78d47c82c12920d24ffea722f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc06f8151c036872d46252d97a8b6fa",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "7a05e5655d72e5d6f9cb",
+    "url": "/react-app/static/js/2.6aae39c0.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/react-app/static/js/2.6aae39c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2adf9a13deb3e17fd4c6",
+    "url": "/react-app/static/js/main.ebd12c8f.chunk.js"
+  },
+  {
+    "revision": "e2c6b0a16d423a796dce",
+    "url": "/react-app/static/js/runtime-main.9a102076.js"
+  }
+]);
